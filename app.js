c3fe63ff664b943f372be8c3c4ce0d91cb821d2b
Andressa Oliveira <andressa.pco@hotmail.com>
2023-04-20 11:28:48 -0300

    resolve problema de cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
 
 const InteressadosRoute = require('./routes/interessados-route');
 
+app.use(cors({ origin: "*" }))
 app.use(bodyParser.urlencoded({ extended: false }));  // apenas dados simples
 app.use(bodyParser.json()); // json de entrada no body
 
